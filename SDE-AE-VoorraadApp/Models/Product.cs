749983e0aaab018441a5e053670eafca37f0db3e
XenoSoul <yoeriwalter@gmail.com>
2021-01-07 14:59:38 +0100

    Db Creation creator and seeder Location and Machine working

--- SDE-AE-VoorraadApp/Models/Product.cs
+++ SDE-AE-VoorraadApp/Models/Product.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace SDE_AE_VoorraadApp.Models
 {
     public class Product
     {
+        [JsonIgnore]
         public int ID { get; set; }
+        [JsonPropertyName("Id")]
         public int ProductID { get; set; }
         public int CategoryID { get; set; }
         public string Name { get; set; }
-        public string ProductSKU { get; set; }
+        public string SkuCode { get; set; }
 
         public ICollection<ProductStock> ProductStocks { get; set; }
         public Category Category { get; set; }
